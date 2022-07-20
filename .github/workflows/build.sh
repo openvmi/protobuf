0c0e2e8b7f2cc273b6cba23568996b0d511427b2
Kai Zhou <zhoukaisspu@163.com>
2022-07-20 23:51:51 +0800

    Update workflow and add build.sh

---
+++ .github/workflows/build.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e 
+
+REPOPATH=${GITHUB_WORKSPACE}
+CURRENT_BRANCH=${CURRENT_BRANCH-"branch-not-available"}
+
+function enterDir {
+    echo "Entering $1"  
+    pushd $1 > /dev/null
+}
+
+function leaveDir {
+    echo "Leaving `pwd`"
+    popd > /dev/null
+}
+
+function buildDir {
+    currentDir="$1"
+    echo "Building directory ${currentDir}"
+    enterDir ${currentDir}
+    buildProtoForTypes $currentDir
+    leaveDir ${currentDir}
+}
+
+function buildProtoForTypes {
+    target=${1%/}
+    echo ${target}
+    if [ -f .protolangs ]; then
+        while read lang; do
+            reponame="protorepo-${target}-${lang}"
+            rm -rf ${REPOPATH}/${reponame}
+            echo "Cloneing repo: https://github.com/openvmi/${reponame}.git"
+            git clone https://github.com/openvmi/${reponame}.git
+        done < .protolangs
+    fi
+}
+
+function buildAll {
+    echo "Build services's protocol buffers"
+    mkdir -p ${REPOPATH}
+    for d in */; do
+        echo $d
+        buildDir $d
+    done
+}
+
+buildAll
