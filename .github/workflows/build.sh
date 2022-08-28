23f7f6011efc40b2dd4d197dfbf44fca4b546e92
Kai Zhou <zhoukaisspu@163.com>
2022-08-28 11:20:38 +0800

    Add registry package and update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -44,6 +44,7 @@ function buildProtoForTypes {
 
 function buildForgo {
     protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./*.proto
+    [ ! -d "$1/pb" ] && mkdir -p "$1/pb"
     cp *.go "$1/pb/"
 }
 
